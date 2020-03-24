bb90180c8f2d7ce4c7b0b552096da474e81969f1
SahibSinghBanga <sahib_singh_1997@hotmail.com>
2020-03-24 23:41:04 +0530

    Updates

---
+++ precache-manifest.a929c75a3e96e37b28f011645fb5415e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4ad61e91a7024040e204ea9ef9afd3d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "32b635bfa3956f685995",
+    "url": "/monsters-rolodex/static/css/main.460421ec.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32b635bfa3956f685995",
+    "url": "/monsters-rolodex/static/js/main.839ba7d9.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
